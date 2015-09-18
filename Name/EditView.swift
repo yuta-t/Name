4320a151d887800bf64cfbaa3c70344f5a70c583
yuta-t <yutatuy.t@gmail.com>
2015-09-18 21:45:33 +0900

    init

---
+++ Name/EditView.swift
@@ -0,0 +1,21 @@
+//
+//  EditView.swift
+//  Name
+//
+//  Created by YutaTadachi on 2015/09/18.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import UIKit
+
+class EditView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Name/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Name
+//
+//  Created by YutaTadachi on 2015/09/18.
+//  Copyright © 2015年 yuta-t. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
