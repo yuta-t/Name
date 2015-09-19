f9e6ed19213956b88ce0e15fbad76c01f29fbbc1
yuta-t <yutatuy.t@gmail.com>
2015-09-20 02:50:15 +0900

    divide layout func

--- Name/HomeView.swift
+++ Name/HomeView.swift
@@ -24,12 +24,18 @@ class HomeView: UIView, EditViewDelegate {
     override func layoutSubviews() {
         super.layoutSubviews()
         backgroundColor = UIColor.whiteColor()
+        layoutNameLabel()
+        layoutTransitButton()
+    }
     
+    private func layoutNameLabel() {
         nameLabel.frame.size = CGSize(width: 200, height: 20)
         nameLabel.center = CGPoint(x: center.x, y: center.y / 2)
         nameLabel.textAlignment = .Center
         nameLabel.text = "No name"
-        
+    }
+    
+    private func layoutTransitButton() {
         transitButton.frame.size = CGSize(width: 100, height: 20)
         transitButton.center = center
         transitButton.setTitle("名前を変更", forState: .Normal)
